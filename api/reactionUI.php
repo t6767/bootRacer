1a28b41eaa2549aece1b5fcc0488655800aec41e
timurlaura729 <53204973+timurlaura729@users.noreply.github.com>
2020-01-27 15:23:22 +0600

    up

--- api/reactionUI.php
+++ api/reactionUI.php
@@ -9,6 +9,7 @@ class reactionUI extends PDO
         // Создаем подключение к БД
         $dns = $settings['database']['driver'].':host=' . $settings['database']['host'].((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '').';dbname='.$settings['database']['schema'];
         parent::__construct($dns, $settings['database']['username'], $settings['database']['password']);
+		echo "77777777777777777";
     }
 
 	function saveToBase($res)
