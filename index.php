6266dfe16014b8b28b916ea9ce7ea1833f05e6d3
timurlaura729 <53204973+timurlaura729@users.noreply.github.com>
2020-01-27 14:33:06 +0600

    up

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
 $postData = file_get_contents('php://input');
+$postData = file_get_contents('php://input');
+$data = json_decode($postData, true);
+// Блок переменных массив делим в переменные
+$idmsg=$data['message']['message_id'];
+$uid=$data['message']['from']['id'];
+$unm=$data['message']['from']['first_name'];
+$dt=date("Y-m-d H:i:s", $data['message']['date']);
+$ms=$data['message']['text'];
+$chatID=$data['message']['chat']['id'];
 var_dump($postData);
 require 'vendor/autoload.php';
 use Telegram\Bot\Api;
 $telegram = new Api('1011237128:AAH5GA5D1yP09M-hgWbc-3fD11RP9vTXgMw');
+$response = $telegram->sendMessage([
+  'chat_id' => $chatID, 
+  'text' => 'Hello World'
+]);
+$messageId = $response->getMessageId();
 $response = $telegram->getMe();
 var_dump($response);
 ?>
