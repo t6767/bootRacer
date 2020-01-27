35b336bb6f6da5bb93fe63a8c0407f8d461c9b8a
timurlaura729 <53204973+timurlaura729@users.noreply.github.com>
2020-01-27 14:22:22 +0600

    up

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-echo "1";
+var_dump($_GET);
+$postData = file_get_contents('php://input');
+var_dump($postData);
 require 'vendor/autoload.php';
 use Telegram\Bot\Api;
 $telegram = new Api('1011237128:AAH5GA5D1yP09M-hgWbc-3fD11RP9vTXgMw');
