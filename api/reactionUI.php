66cea34d2e31441b0dbb50d93230ea72d74bf5c8
timurlaura729 <53204973+timurlaura729@users.noreply.github.com>
2020-01-27 17:39:26 +0600

    start

--- api/reactionUI.php
+++ api/reactionUI.php
@@ -59,6 +59,7 @@ class reactionUI extends PDO
                     $reply .= "\xE2\x9E\xA1 <b>ТитулXXX</b> <i>$i</i> (<a href='https://mail.ru/'>читать</a>)\n";
                 }
                 $this->sendHTML($reply);
+                $this->sendMSG($reply);
                 break;
 			default:
                 {
