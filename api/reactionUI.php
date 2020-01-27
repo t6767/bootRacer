9b693d80596ccdab66f23fe3831499b6b7044e1b
timurlaura729 <53204973+timurlaura729@users.noreply.github.com>
2020-01-27 18:01:18 +0600

    start

--- api/reactionUI.php
+++ api/reactionUI.php
@@ -51,7 +51,7 @@ class reactionUI extends PDO
 				break;
             case "stop" :
                 $reply = "Добро бота!";
-                $reply_markup = $this->telegram->replyKeyboardMarkup([ 'keyboard' => $this->keyboard2]);
+                $reply_markup = $this->telegram->replyKeyboardMarkup([ 'inline_keyboard' => $this->keyboard]);
                 $this->sendMSGRepl($reply, $reply_markup);
                 break;
             case "Картинка":
