1107854d3a424b377f9f36694853046d1f5aa747
timurlaura729 <53204973+timurlaura729@users.noreply.github.com>
2020-01-28 12:40:10 +0600

    start

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ date_default_timezone_set("Asia/Almaty");
 $postData = file_get_contents('php://input');
 $data = json_decode($postData, true);
 var_dump($postData);
-require_once("api/reactionUIChanal.php");
-$reactionUI = new reactionChanal('417445048:AAFx37CoDNNItBN0NHL3xj5TaBXNSNXqzgM', $data,'my_setting.ini');
+require_once("api/reactionUI.php");
+$reactionUI = new reactionUI('417445048:AAFx37CoDNNItBN0NHL3xj5TaBXNSNXqzgM', $data,'my_setting.ini');
 ?>
